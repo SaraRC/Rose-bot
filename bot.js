393a7da536431a86be04c43e7bb3aee7b9e2e5e6
Sara <srcellucci@gmail.com>
2021-01-26 17:50:48 -0500

    WIP: switch to individual command files
    Moved ping to individual files, just need to do the same for the other commands.

--- bot.js
+++ bot.js
@@ -1,13 +1,28 @@
-//Run dotenv
 
+//Requires =========================================
 require('dotenv').config();
+const fs = require('fs');
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
 const auth = require('./auth.json');
+//const commands = require('./commands.js');
 
-const commands = require('./commands.js');
+//Here be globals ==================================
+const client = new Discord.Client();
+const commands = new Discord.Collection();
+
+//Grab all the command file names and put them in a list.
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+//Run through the list and put each one's exports object into the list
+for (file of commandFiles) {
+	console.log(`Found file ${file}`);
+	const command = require(`./commands/${file}`);
+	console.log(`Requiring ${command.name}`);
+	//Put each in the collection with the name property as its key so they can be directly addressed by name.
+	commands.set(command.name, command);
+}
 
 
 client.on('ready', () => {
@@ -16,7 +31,11 @@ client.on('ready', () => {
 
 client.on('message', msg => {
 	if (msg.content.startsWith(process.env.COMMAND_PREFIX)) {
-		commands[msg.content.slice(1, msg.content.length)](msg);
+		//commands[msg.content.slice(1, msg.content.length)](msg);
+		//TODO: Actual error handling for nonexistant command names.
+		//Get the command and call its execute method.
+		console.log(msg.content.slice(process.env.COMMAND_PREFIX.length, msg.content.length));
+		commands.get(msg.content.slice(process.env.COMMAND_PREFIX.length, msg.content.length)).execute(msg, null);
 	}
 	
 });
